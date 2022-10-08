e91b3c9166531e081bab2bcc2a25a7bbe29e6f58
marlibon <marlic@list.ru>
2022-10-08 13:18:17 +0500

    verstka: header, menu, kartochka

---
+++ index.js
@@ -0,0 +1,27 @@
+let phrases = [
+    { text: 'сочные_бургеры' },
+    { text: 'аппетитная_пицца' },
+    { text: 'потрясная_шаурма' },
+    { text: 'вкусная_выпечка' },
+    { text: 'быстрая_доставка' },
+    { text: 'то,_что_мы_любим :)' }
+  ];
+  let randIndex = 0;
+  function getRandomElement(arr) {
+    randIndex++;
+    if (randIndex > 5) { 
+      randIndex = 0 
+    }
+    else { }
+    return arr[randIndex];
+  }
+  
+  let phrase = document.querySelector('.phrase');
+  
+  setInterval(function () {
+    let randomElement = getRandomElement(phrases);
+    phrase.innerHTML = '<div class="phrase-animation">' + randomElement.text + '</div>';
+  }, 4000);
+
+  
+  
